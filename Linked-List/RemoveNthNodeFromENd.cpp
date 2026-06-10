98e0876fb50883bed20ba83882f2bdaf4173b0aa
unknown <paemjeetk@gmail.com>
2026-06-11 03:26:59 +0530

    day10

---
+++ Linked-List/RemoveNthNodeFromENd.cpp
@@ -0,0 +1,36 @@
+//opyimal--------------------
+//o(n) -- single pass
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(!head) return NULL;
+        
+        ListNode dummy(-1, head);
+        ListNode* slow = &dummy;
+        ListNode* fast = &dummy;
+
+        for(int i=0; i<=n; i++) fast = fast -> next;
+
+        while(fast){
+            slow = slow -> next;
+            fast = fast -> next;
+        }
+    
+        ListNode* Node2delete = slow -> next;
+        slow -> next = slow -> next -> next;
+        delete Node2delete;
+
+        
+        return dummy.next;
+    }
+};
