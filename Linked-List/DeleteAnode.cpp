2161a8ef4bf3c029ce516f8aebc8a06413ff4009
unknown <paemjeetk@gmail.com>
2026-06-11 03:25:58 +0530

    day10

---
+++ Linked-List/DeleteAnode.cpp
@@ -0,0 +1,20 @@
+//-------------optimal--------------
+//tc = O(1) sc = O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        ListNode* curr = node;
+       
+        curr -> val = curr -> next -> val;
+        curr -> next = curr -> next -> next;
+    }
+};
