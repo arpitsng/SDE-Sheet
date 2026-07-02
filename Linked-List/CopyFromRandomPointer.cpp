14f5a52351ac99daf5e24e65c8e46ef4f2c949ac
unknown <paemjeetk@gmail.com>
2026-07-03 01:35:09 +0530

    Brute Force

---
+++ Linked-List/CopyFromRandomPointer.cpp
@@ -0,0 +1,45 @@
+//--------------Brute FOrce------------
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return head;
+
+        unordered_map<Node*, Node*> copy;
+
+        Node* temp = head;
+        while(temp){
+            copy[temp] = new Node(temp -> val);
+            temp = temp -> next;
+        }
+
+        temp = head;
+        while(temp){
+            if(temp -> random) {
+                copy[temp] -> random = copy[temp -> random];
+                
+            }
+            else copy[temp] -> random = NULL;
+            copy[temp] -> next = copy[temp -> next];
+
+            temp = temp -> next;
+        }
+
+        return copy[head];
+    }
+};
