448152dafca741251de2c8cb6803f67d476333a6
unknown <paemjeetk@gmail.com>
2026-07-06 03:31:44 +0530

    optimal

--- Recursion/PermutationSequence.cpp
+++ Recursion/PermutationSequence.cpp
@@ -28,3 +28,36 @@ public:
         }
     }
 };
+
+
+//--------------------optimal-----------------------
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> num;
+        int fact = 1;
+
+        for (int i = 1; i < n; i++)
+            fact *= i;
+
+        for (int i = 1; i <= n; i++)
+            num.push_back(i);
+
+        k--;
+
+        string ans = "";
+
+        while (true) {
+            ans += to_string(num[k / fact]);
+            num.erase(num.begin() + k / fact);
+
+            if (num.empty())
+                break;
+
+            k %= fact;
+            fact /= num.size();
+        }
+
+        return ans;
+    }
+};
