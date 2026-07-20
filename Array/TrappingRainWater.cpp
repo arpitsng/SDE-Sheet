cd76b8b15b46bc2a3b0e7b5a41e30d59104c75a8
arpitsng <paemjeetk@gmail.com>
2026-07-21 04:08:45 +0530

    ended with optimal

--- Array/TrappingRainWater.cpp
+++ Array/TrappingRainWater.cpp
@@ -1,3 +1,33 @@
+//--------------optimal---------------
+    class Solution {
+    public:
+        int trap(vector<int>& height) {
+            int n= height.size();
+           int left = 0, right = n-1;
+           int leftmax = 0, rightmax = 0;
+
+            int ans = 0;
+            while(left < right){
+                if(height[left] <= height[right]){
+                    if(leftmax > height[left]) ans += leftmax - height[left];
+                    else leftmax = height[left];
+
+                    left++;
+                }
+
+                else{
+                    if(rightmax > height[right]) ans += rightmax - height[right];
+                    else rightmax = height[right];
+
+                    right--;
+                }
+            }
+
+            return ans;
+        }
+    };
+
+    
 //-----better approach------------
 class Solution {
 public:
